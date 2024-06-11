9621aaeb2779305f2adff762ba8792158777b665
Diana Jimenez Roo <149723555+DianaJRoo@users.noreply.github.com>
2024-06-11 06:28:49 +0000

    cambios

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -17,41 +17,39 @@ const Card = ({ images , title, description1, buttonLink }) => {
   };
 
   return (
-    <div id='hero' className='d-flex flex-column mx-auto px-4 md:px-8 lg:px-16 xl:px-20 py-20 p-20 w-full'>
-      <div className="flex flex-row bg-neutral-100 border border-gray-200 rounded-lg overflow-hidden mt-1 mb-1 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-xl" style={{ width: '50rem', height: '20rem' }}>
-        <div className="relative">
-          <img className="w-full h-full object-cover" src={images[currentImageIndex]} alt={title} />
-          
-          <button
-            className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-500 bg-opacity-50 hover:bg-gray-700 text-white p-2 rounded-full"
-            onClick={handlePrevClick}
-          >
-            ◀
-          </button>
-          <button
-            className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-500 bg-opacity-50 hover:bg-gray-700 text-white p-2 rounded-full"
-            onClick={handleNextClick}
-          >
-            ▶
-          </button>
+    <div id='hero' className='flex flex-col justify-center items-center mx-auto px-4 md:px-8 lg:px-16 xl:px-20 py-20'>
+    <div className="flex flex-col md:flex-row bg-neutral-100 border border-gray-200 rounded-lg overflow-hidden mt-1 mb-1 transform hover:scale-110 transition-transform duration-300 ease-in-out hover:shadow-xl" style={{ width: '50rem', height: '20rem' }}>
+        <div className="relative w-full md:w-1/2">
+            <img className="w-full h-full object-cover" src={images[currentImageIndex]} alt={title} />
+            
+            <button
+                className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-500 bg-opacity-50 hover:bg-gray-700 text-white p-2 rounded-full"
+                onClick={handlePrevClick}
+            >
+                ◀
+            </button>
+            <button
+                className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-500 bg-opacity-50 hover:bg-gray-700 text-white p-2 rounded-full"
+                onClick={handleNextClick}
+            >
+                ▶
+            </button>
         </div>
-        <div className="w-1/2 px-6 py-4 flex flex-col justify-between">
-        <div className=" w-full px-6 py-4 flex-1">
-          <div className="font-bold text-l mb-2 text-center w-full ">{title}</div>
-          <p className="text-gray-700 text-base flex justify-center flex-1">
-            <strong>{description1}</strong>
-          </p>
-          </div>
-          <div className="px-8 mt-2 pt-2 pb-2 flex justify-center">
-            <Link to={buttonLink} rel="noopener noreferrer">
-              <button className="mt-auto justify-center rounded-md bg-pink-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Link
-              </button>
-            </Link>
-          </div>
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center px-6 py-4">
+            <div className="font-bold text-l mb-2 text-center w-full">{title}</div>
+            <p className="text-gray-700 text-base text-center">
+                <strong>{description1}</strong>
+            </p>
+            <div className="mt-4">
+                <a href={buttonLink} target="_blank" rel="noopener noreferrer">
+                    <button className="rounded-md bg-pink-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Link
+                    </button>
+                </a>
+            </div>
         </div>
-      </div>
     </div>
+</div>
   );
 };
 
